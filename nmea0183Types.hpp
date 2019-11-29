c5019ea0c25c9e52a24cb6cd6030d43562e8c153
Sylvain <sylvain.joyeux@tidewise.io>
2019-11-29 19:07:58 -0300

    initial commit by rock-create-orogen

---
+++ nmea0183Types.hpp
@@ -0,0 +1,15 @@
+#ifndef nmea0183_TYPES_HPP
+#define nmea0183_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace nmea0183 {
+}
+
+#endif
+
